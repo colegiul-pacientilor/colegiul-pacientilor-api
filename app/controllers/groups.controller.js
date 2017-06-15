b3e27339d9cb3b2ddc78ae00cccb2407f2116ef0
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-06-15 16:25:42 +0300

    Add user to group API endpoint

--- app/controllers/groups.controller.js
+++ app/controllers/groups.controller.js
@@ -20,7 +20,7 @@ routes.post('/groups', function (req, res) {
 
 // Get all groups
 routes.get('/groups', function (req, res) {
-  Group.find({}, function(err, groups) {
+  Group.find({}, function (err, groups) {
     res.send(groups);
   });
 });
@@ -34,9 +34,27 @@ routes.get('/groups/:id', function (req, res) {
 
 // Remove Group by id
 routes.delete('/groups/:id', function (req, res) {
-  Group.find({ _id: req.params.id }).remove().exec();
+  Group.find({_id: req.params.id}).remove().exec();
   res.send({success: true});
 });
 
+// Add user to group
+routes.post('/groups/:id/users', function (req, res) {
+  Group.findById(req.params.id, function (err, group) {
+    group.users.push({
+      email: req.body.user.email,
+      firstName: req.body.user.firstName,
+      lastName: req.body.user.lastName
+    });
+
+    group.save(function (err) {
+      if (err) {
+        res.send({error: true});
+      }
+
+      res.send(group);
+    });
+  });
+});
 
 module.exports = routes;
