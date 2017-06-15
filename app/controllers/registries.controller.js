892299a0f334fa9e9b65162b004df31659cc515d
georgebuzoianu <buzoianu_george@yahoo.com>
2017-06-16 01:57:38 +0300

    Add endpoint for uploading excel file

--- app/controllers/registries.controller.js
+++ app/controllers/registries.controller.js
@@ -3,7 +3,9 @@
 // 2. handle registry states (invalid, deleted/archived, draft)
 // 3. handle version
 
-
+var multer = require('multer');
+var xlstojson = require("xls-to-json-lc");
+var xlsxtojson = require("xlsx-to-json-lc");
 
 const express = require('express'),
   routes = express.Router(),
@@ -159,6 +161,78 @@ function RepositoryElasticsearchService(client) {
   }
 
 }
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage,
+    fileFilter : function(req, file, callback) { //file filter
+        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+            return callback(new Error('Wrong extension type'));
+        }
+        callback(null, true);
+    }
+}).single('file');
+
+routes.post('/registries/:id/upload', function(req, res) {
+    var exceltojson;
+    upload(req,res,function(err){
+        if(err){
+            res.json({error_code:1,err_desc:err});
+            return;
+        }
+        /** Multer gives us file info in req.file object */
+        if(!req.file){
+            res.json({error_code:1,err_desc:"No file passed"});
+            return;
+        }
+        /** Check the extension of the incoming file and
+         *  use the appropriate module
+         */
+        if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
+            exceltojson = xlsxtojson;
+        } else {
+            exceltojson = xlstojson;
+        }
+        console.log("File " +req.file.originalname+ " was uploaded to following path: " + req.file.path);
+        try {
+            console.log("Trying to parse excel file....");
+            exceltojson({
+                input: req.file.path,
+                output: null, //since we don't need output.json
+                lowerCaseHeaders:true
+            }, function(err,result){
+                if(err) {
+                    return res.json({error_code:1,err_desc:err, data: null});
+                }
+                console.log("HOORAY!!!! Excel file was successfully parsed");
+                console.log("The file contains " + result.length + " cases. Start to create cases");
+
+                Registry.findById(req.params.id, function (err, registry) {
+                    console.log("Registry " + req.params.id + " was found in db");
+                    registry.records.push({
+                        values: result
+                    });
+
+                    res.send(registry);
+                });
+                //res.json({error_code:0,err_desc:null, data: registry});
+            });
+        } catch (e){
+            res.json({error_code:1,err_desc:"Corupted excel file"});
+        }
+    })
+
+});
+
 
 
 module.exports = routes;
