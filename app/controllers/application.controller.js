9cddb93472c5faf992dd9f1b60ab18f8e46f2874
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-06-15 16:14:29 +0300

    Clear application controller

--- app/controllers/application.controller.js
+++ app/controllers/application.controller.js
@@ -1,32 +1,4 @@
 const express = require('express'),
-      routes = express.Router(),
-      Group = require('../models/group.model');
-
-// routes.get('/groups/add', function (req, res) {
-//   var s = new Group({
-//     name: 'Group 2',
-//     description: 'Lorem ipsum dolor'
-//   });
-//
-//   s.save(function (err) {
-//     if (err) {
-//       res.send({error: true});
-//     }
-//
-//     res.send(s);
-//   });
-// });
-//
-// routes.get('/groups', function (req, res) {
-//   Group.find({}, function(err, groups) {
-//     // var groupMap = {};
-//     //
-//     // groups.forEach(function(group) {
-//     //   groupMap[group._id] = group;
-//     // });
-//
-//     res.send(groups);
-//   });
-// });
+      routes = express.Router();
 
 module.exports = routes;
